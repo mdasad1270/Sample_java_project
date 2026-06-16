14788b2441680fec18e227e92215799444854410
mdasad1270 <148677457+mdasad1270@users.noreply.github.com>
2026-06-16 14:47:48 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,6 +2,6 @@ public class Demo{
     public static void main (String [] args){
     System.out.println ("Hello from Demo");
         System.out.println ("Hello from E28");
-        
+        System.out.println ("Hello Asad");
         }
 }
